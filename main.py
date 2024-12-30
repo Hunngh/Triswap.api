f31871333c23738210760da0cf831fd0868935dd
Hunngh <wtffish@qq.com>
2024-12-31 00:57:43 +0800

    新增CORS的跨域冲突，以及添加了管理员路由

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-from app.routers import user_router
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from app.routers import user_router, admin_router
 from app.database.database import Base, engine, get_db
 
 # 初始化数据库
@@ -7,8 +8,18 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Triswap API", description="API for Triswap application", version="0.1.0")
 
+# 配置 CORS 中间件
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://120.46.200.190"],  # 允许的源
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许的 HTTP 方法
+    allow_headers=["*"],  # 允许的 HTTP 头
+)
+
 # 注册路由
 app.include_router(user_router.router)
+app.include_router(admin_router.router)
 
 @app.get("/")
 async def root():
